305254783bd97ceb20417d8006d023559192456f
jorawaraksk <jorawarzu@proton.me>
2024-09-22 15:27:25 +0530

    Update save.py

--- TechVJ/save.py
+++ TechVJ/save.py
@@ -66,10 +66,10 @@ def progress(current, total, message, type):
 @Client.on_message(filters.command(["start"]))
 async def send_start(client: Client, message: Message):
     buttons = [[
-        InlineKeyboardButton("𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭", url = "https://t.me/Farooq_is_King")
+        InlineKeyboardButton("SHINchan", url = "https://t.me/jdkskssbsbjdjdkss")
     ],[
-        InlineKeyboardButton('🔍 WD Topic Group', url='https://t.me/Op_Topic_Group'),
-        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/Opleech_WD')
+        InlineKeyboardButton(' Mitsi nohara', url='https://t.me/Odjdjsjsbdhdisksksksks'),
+        InlineKeyboardButton('🤖 Himawari', url='https://t.me/OjdjskssksnnsnsksksskD')
 	]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(message.chat.id, f"<b>👋 Hi {message.from_user.mention}, I am Save Restricted Content Bot, I can send you restricted content by its post link.\n\nFor downloading restricted content /login first.\n\nKnow how to use bot by - /help</b>", reply_markup=reply_markup, reply_to_message_id=message.id)
